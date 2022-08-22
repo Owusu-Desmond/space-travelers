03ef3d73617428f85fe0fd04e14c11d18b91331c
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-22 16:17:54 +0200

    setup redux folder for missions

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const FETCH_MISSIONS = 'missions/fetch_missions';
+
+export default function missionsReducer(state = [], action) {
+  if (action.type === `${FETCH_MISSIONS}/fulfilled`) {
+    return action.payload
+  }
+}
+
+export const addMissions = createAsyncThunk(
+  FETCH_MISSIONS,
+  async () => {
+    const res = await axios.get('https://api.spacexdata.com/v3/missions');
+    const data = res.data;
+    return { data };
+  }
+)
