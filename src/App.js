a9145d1a27214838c96d59f3c080c2b2e1aa12e1
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-23 13:39:35 +0000

    Include rocket page in app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './styling/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
-import store from './redux/configureStore';
+import RocketPage from './components/rocketPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Navbar />
-        <div className="App-content">
-          <Routes>
-            <Route path="/" element={<h1>this is my rockets page</h1>} />
-            <Route path="/missions" element={<h2>this is my missions page</h2>} />
-            <Route path="/profile" element={<h3>this is my profile page</h3>} />
-          </Routes>
-        </div>
-      </Provider>
+      <Navbar />
+      <hr />
+      <div className="App-content">
+        <Routes>
+          <Route path="/" element={<RocketPage />} />
+          <Route path="/missions" element={<h2>this is my missions page</h2>} />
+          <Route path="/profile" element={<h3>this is my profile page</h3>} />
+        </Routes>
+      </div>
     </BrowserRouter>
 
   );
