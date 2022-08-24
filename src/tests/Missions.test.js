0e86e90f68ebb42dcbad959be16d72f79aad5a91
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-24 16:36:00 +0200

    add tests for the reducer

--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,56 +1,60 @@
-import React from "react";
-import {rest} from 'msw'
-import {setupServer} from 'msw/node'
-import {fireEvent, render, screen} from '@testing-library/react'
-import { Provider } from "react-redux";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import Missions from '../components/Missions';
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import missionsReducer, { addMissions } from "../redux/missions/missions";
-import axios from "axios";
+import missionsReducer from '../redux/missions/missions';
 
-
-// const server = setupServer(
-//   rest.get('https://api.spacexdata.com/v3/missions', (req, res, ctx) => {
-//     return res(ctx.json({
-//       mission_id: '99',
-//       mission_name: 'mission1',
-//       description: 'really cool mission',
-//     }))
-//   })
-// )
+const initialState = {
+  missions: [{
+    id: '99',
+    name: 'mission1',
+    description: 'really cool mission',
+  }],
+};
 
 const rootReducer = combineReducers({
-  missions: missionsReducer
-})
+  missions: missionsReducer,
+});
 
 const store = configureStore({
   reducer: rootReducer,
-  preloadedState: {
-    missions: [{
+  preloadedState: initialState,
+});
+
+describe('component tests', () => {
+  it('renders', () => {
+    const missions = render(<Provider store={store}><Missions /></Provider>);
+
+    expect(missions).toMatchSnapshot();
+  });
+
+  it('displays state from the store to the UI', () => {
+    render(<Provider store={store}><Missions /></Provider>);
+
+    expect(screen.getByText('really cool mission')).toBeInTheDocument();
+  });
+});
+
+describe('reducer logic tests', () => {
+  test('the reducer should return inital state', () => {
+    expect(missionsReducer(initialState, { type: undefined })).toBe(initialState);
+  });
+
+  test('the reducer should return the new reserved value', () => {
+    const initialState = [{
       id: '99',
       name: 'mission1',
       description: 'really cool mission',
-    }],
-  }
+    }];
+
+    const newState = [{
+      id: '99',
+      name: 'mission1',
+      description: 'really cool mission',
+      reserved: true,
+    }];
+
+    expect(missionsReducer(initialState, { type: 'missions/add_joining', id: '99' })).toStrictEqual(newState);
+  });
 });
-
-// // establish API mocking before all tests
-// beforeAll(() => server.listen())
-// // reset any request handlers that are declared as a part of our tests
-// // (i.e. for testing one-time error scenarios)
-// afterEach(() => server.resetHandlers())
-// // clean up once the tests are done
-// afterAll(() => server.close())
-
-it('renders', () => {
-  const missions = render(<Provider store={store}><Missions /></Provider>);
-
-  expect(missions).toMatchSnapshot();
-})
-
-it('displays state from the store to the UI', () => {
-  (<Provider store={store}><Missions /></Provider>);
-  screen.debug()
-
-  expect(screen.findByText('really cool mission').toBeInTheDocument)
-})
